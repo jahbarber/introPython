746fef7a9f9aba941a6826f67f9d2fd4d1fe8026
S <144710839+jahbarber@users.noreply.github.com>
2023-09-25 15:08:39 +0000

    finish homework

---
+++ introPythonActivities/functionStarterActivity.py
@@ -0,0 +1,21 @@
+
+# 1. In your own words, describe what a function is
+A function is a reusable piece code that performs a certain task
+# 2. What is are function parameters and arguments and describe
+# the difference between the two. 
+A function parameter is a placeholder and a function argument is the actual data.
+# 3. write a function that will print out a welcome message
+# that includes a users name. You will need to use parameters and arguments
+
+user_Name = "Jahmere Barber"
+def welcome_msg(): 
+    print(user_Name,welcome to class.)
+welcome_msg()
+
+# 4. Write a function that will do a calculation that takes 3 parameters.
+# Your function can do any of the arithmatic operators (add, subtract, multiply, divide)
+    def
+# 5. Write a function that will output a message to a user, telling them
+# what class they have next after this one. this code should use a 
+# variable to pass a value into the parameter of a function. The variable should
+# be real, user data- not hard coded data.  
