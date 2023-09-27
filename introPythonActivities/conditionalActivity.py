7f7863f627827a449ae993daeb4727c2da4434ad
S <144710839+jahbarber@users.noreply.github.com>
2023-09-27 17:02:03 +0000

    finished classwork

---
+++ introPythonActivities/conditionalActivity.py
@@ -0,0 +1,38 @@
+# 1. What is the difference between 
+# a parameter and an argument in a python function
+
+"An argument is the data inside the round brackets and a parameter is the round brackets"
+
+# 2. In your own words, describe what 
+# a conditional statement (if/else) is 
+
+"A conditional statement is an if or else statement which is when somebody does a specific function that is in the instructions do this or do not do anything"
+"if money is taken out of an atm-give them physical money."
+"else-don't give them money."
+
+# 3. write a simple conditional state using a comparision operator(greater than, less than, etc. )
+if:amount_of_money > amount_of_money_for_candybar:
+    
+'else alert the person that they don't have enough for the candy bar'  
+
+
+# 4. Write a conditional statement for food in the refridgerator.
+# your conditional statement should be wrapped in a function that takes one (1)
+# parameter. The data type for this parameter should be true or false. 
+# your function should have a line of code that asks the user if there is food in the refridgerator.
+# If there is food in the refridgerator, print time to cook. If there is 
+# NOT food in the fridge, print time to shop. 
+# When you call your function there should be an argument that accepts 
+# a boolean. 
+def food_in_the_refidgerator():
+
+if food_in_the_refidgerator=True
+    print(time to cook)
+else food_in_the_refidgerator=False
+    print(time to shop)
+food_in_the_refidgerator(True)
+# 5. Create a function that checks the inventory of cereal for a store. 
+# your function should have a parameter that accepts an integer. In your function
+# use a conditional statement to determine if you need to order more cereal.
+# If there is more than 10 boxes, print "inventory full", else if there are less than 
+# 10 boxes print "we need to order more cereal".
---
+++ introPythonActivities/jeopardy.py
@@ -0,0 +1,10 @@
+# create a function that will take in a user's picture, name, and city/ location and
+#then print out those values. This will be used for their profile on our app.
+
+#we'll be using input() to ask the user questions
+#we'll use print
+
+def profileData(picture, name, city):
+    print(picture + name + city)
+
+profileData()
