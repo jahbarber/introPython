7d128f1dff227742ffd0607231470629972308bf
S <144710839+jahbarber@users.noreply.github.com>
2023-10-18 16:56:55 +0000

    Loop practice work

---
+++ introPythonActivities/loopPractice.py
@@ -0,0 +1,43 @@
+# Define While -
+# Loops are function that use the while keyword
+#  We need to give it instructions, like any other function
+# keeps a program running so long as it is true.
+
+
+#i=0
+#while i < 100:
+#    print('we are learning about loops')
+#    break
+    #i+=1
+#print('our loop is done')
+
+# break
+
+# continue- skips the condition specified in your program.
+def continueExample():
+    i=0
+    while i <= 8:
+       
+        i+=1
+        if i==5:
+            print('skip the number 5')
+            continue
+        print(i)
+
+ # print('our loop is done.)
+
+
+continueExample()    
+   
+
+#i = 0
+#while i < 9:
+#    i += 1
+#    if i == 3:
+#        continue
+#    print()
+
+
+
+
+# else
