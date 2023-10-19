4c48b5b1e942a98bcc349f2f99670bb045c56c06
S <144710839+jahbarber@users.noreply.github.com>
2023-10-19 16:30:22 +0000

    Loop work

--- introPythonActivities/loopReviewActivityHw.py
+++ introPythonActivities/loopReviewActivityHw.py
@@ -3,15 +3,15 @@
 # 1. Create a security camera program that uses a while loop to detect if there is an
 # object in site. 
 
-def security_camera_people():
-    people_On_Camera = 0
-    while people_On_Camera < 1 :
+#def security_camera_people():
+#    people_On_Camera = 0
+#    while people_On_Camera < 1 :
         
    
-        people_On_Camera +=1
-    if people_On_Camera>=1:
-        print('Alert, people on camera!')
-security_camera_people()
+#        people_On_Camera +=1
+#    if people_On_Camera>=1:
+#        print('Alert, people on camera!')
+#security_camera_people()
 
 
 
@@ -19,17 +19,37 @@ security_camera_people()
 # 2. Create a Printer Loop program that will continue to print copies of a document based on the number
 # that the user inputs. 
 
+Document_number = 1
+copies_Of_Document= 3
+
+while copies_Of_Document >1:
+
+    if Document_number >=1:
+        print('Here are your copies.')
+    break
+
+
 # 3. Create a Stop Light Loop that will change the light color based on different time intervals. 
 # every 30 seconds the light should change between green and red. 
 
-Light_for_stoplight=
-go_Light=30
-slowdown_Light=30
-stop_Light=30
-while Light_for_stoplight >= 30:
+Light_for_stoplight= ( 'go_Light + slowdown_Light + stop_Light')
+go_Light='30'
+slowdown_Light='30'
+stop_Light='30'
+while Light_for_stoplight >= '90':
 
-    if stop_Light >= 30:
-        print('Change light')
+    if go_Light >='30':
+        print('Change light 1.')
+    
+    if slowdown_Light>= '30':
+        print('Change light 2.')
+
+
+    if stop_Light >= '30':
+        print('Change light 3')
+    else:
+        print('Go')
+    break
 
 
 
