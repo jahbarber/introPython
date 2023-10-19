9754ca8819c7fe027e3a0cb7a51f2d49641dcddb
S <144710839+jahbarber@users.noreply.github.com>
2023-10-19 23:08:58 +0000

    Homework complete

--- introPythonActivities/loopReviewActivityHw.py
+++ introPythonActivities/loopReviewActivityHw.py
@@ -3,15 +3,15 @@
 # 1. Create a security camera program that uses a while loop to detect if there is an
 # object in site. 
 
-#def security_camera_people():
-#    people_On_Camera = 0
-#    while people_On_Camera < 1 :
+def security_camera_people():
+    people_On_Camera = 0
+    while people_On_Camera < 1 :
         
    
-#        people_On_Camera +=1
-#    if people_On_Camera>=1:
-#        print('Alert, people on camera!')
-#security_camera_people()
+        people_On_Camera +=1
+    if people_On_Camera>=1:
+       print('Alert, people on camera!')
+security_camera_people()
 
 
 
@@ -50,6 +50,22 @@ while Light_for_stoplight >= '90':
     else:
         print('Go')
     break
+
+# 4. Think of a scenario where you would use a while loop and the break keyword. 
+# Then write down and describe it.  
+# This does not need to be a code example.
+
+# When you make a stop light program and you use after every 30 seconds with the go light, the slow 
+# down light and stop light.
+
+
+# 5. Think of a  scenario where you would use a while loop and a Create a while and the continue keyword.
+# Then write down and describe it.  
+# This does not need to be a code example.
+
+#When a fan is turned on it goes on continuously without stopping till you turn it off.
+
+
 
 
 
