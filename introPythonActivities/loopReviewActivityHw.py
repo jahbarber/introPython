3dcf474bbecef0f2f987b893851bff80d29db78d
S <144710839+jahbarber@users.noreply.github.com>
2023-10-19 16:17:48 +0000

    Loop work

---
+++ introPythonActivities/loopReviewActivityHw.py
@@ -0,0 +1,40 @@
+# Create while loops for the following conditions
+
+# 1. Create a security camera program that uses a while loop to detect if there is an
+# object in site. 
+
+def security_camera_people():
+    people_On_Camera = 0
+    while people_On_Camera < 1 :
+        
+   
+        people_On_Camera +=1
+    if people_On_Camera>=1:
+        print('Alert, people on camera!')
+security_camera_people()
+
+
+
+
+# 2. Create a Printer Loop program that will continue to print copies of a document based on the number
+# that the user inputs. 
+
+# 3. Create a Stop Light Loop that will change the light color based on different time intervals. 
+# every 30 seconds the light should change between green and red. 
+
+Light_for_stoplight=
+go_Light=30
+slowdown_Light=30
+stop_Light=30
+while Light_for_stoplight >= 30:
+
+    if stop_Light >= 30:
+        print('Change light')
+
+
+
+
+
+
+# 3. BONUS: add an additional condition that will change the light to yellow for 5 seconds before the
+# next light change. 
