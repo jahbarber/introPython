5419b608fefcbc40b5ef740c4442496fbe82c963
S <144710839+jahbarber@users.noreply.github.com>
2023-10-24 16:08:57 +0000

    Warmup done

---
+++ introPythonActivities/forLoopLesson.py
@@ -0,0 +1,18 @@
+# WHILE Loop - is a keyword that will repeat a block of code
+# indefinately, so long as the condition is true.
+
+# FOR Loop - Is a key that will repeat a block of code, 
+# but only to the limit of the array/list.
+# THIS DOES NOT RUN INDEFINITELY.
+
+zoo= ['tiger','bear','eagle','otter','dolphin']
+exampleList2=['word',10,11.1212,True,zoo]
+
+var='ian'
+num=2332
+
+profile= [ name= 'Ian'
+            age=32,
+            career='engineer'
+            city='Philly'
+            canDrive=True,]
---
+++ introPythonActivities/forLoopsWarmupActivity.py
@@ -0,0 +1,15 @@
+# 1. In your own words, describe what a for loop is?
+# A for loop is a loop that use the for function and use square brackets.
+# 2. What is the difference between a FOR loop and a WHILE loop?
+# Provide two (2) examples of each. 
+#A while loop runs indefinately but a for loop doesn't.
+#While loop-A printer and stop light
+#For loop-A passcode and An engine.
+# 3. Create a FOR loop that will go through a list of names 
+# and print all the names that start with the letter "R".
+names_with_r='Rebecca','Robert', 'Rose'
+names=['Michael','Rebecca','William','Kareem','Robert','Rose','Jason']
+for names in names_with_r :
+    if names_with_r := ('Rebecca, Robert,Rose'):
+        print (names_with_r, 'are your names')
+
