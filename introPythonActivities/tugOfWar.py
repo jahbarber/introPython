a216a85974ab5091ef08278fddf2ef9eb98fdb7f
S <144710839+jahbarber@users.noreply.github.com>
2023-10-27 16:14:44 +0000

    Cesium questions finished

--- introPythonActivities/tugOfWar.py
+++ introPythonActivities/tugOfWar.py
@@ -1,13 +1,3 @@
 
 
-
-def hot_Cold_Temperature():
-    temperature= input('What is the temperature.')
-
-    if temperature<75.00:
-        if temperature> 70.00:    
-            print('weather is perfect.')
-    else:
-        print('it is hot outside.')
-        print('it is cold outside.')
-hot_Cold_Temperature()
+print('W')
