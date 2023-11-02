931fc24dc1e09fb5d8252c2f134ee2dae4978d6e
S <144710839+jahbarber@users.noreply.github.com>
2023-11-02 23:05:29 +0000

    My program completed

---
+++ introPythonActivities/Clone_assignment/jahmereBarber_GitCloneFunction.py
@@ -0,0 +1,8 @@
+videogame=10
+
+def amount_for_games():
+    if videogame>0:
+        print('You have one or more video games.')
+    else:
+        print('You have no video games.')
+amount_for_games()
